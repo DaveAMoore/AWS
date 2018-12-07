471f842844cff31a31a9b752afabaeffb1caf159
David Moore <mooredev@me.com>
2018-12-06 20:48:52 -0500

    Remove UIKit Import

--- AWSIoT/AWSIoT.h
+++ AWSIoT/AWSIoT.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Moore Development. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for AWSIoT.
 FOUNDATION_EXPORT double AWSIoTVersionNumber;
